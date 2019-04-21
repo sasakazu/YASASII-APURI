a1b5c781d5aa1834d0da4e088f749cbbe7202e48
kazuyasasakura <kazuya.sasakura@gmial.com>
2019-04-21 18:19:48 +0900

    alert done...

--- YASASHII-APURI/todayFeeling.swift
+++ YASASHII-APURI/todayFeeling.swift
@@ -11,11 +11,20 @@ class todayFeeling: UIViewController {
     
     @IBOutlet weak var todayDate: UILabel!
     @IBOutlet weak var feelingImage: UIImageView!
+    
     @IBOutlet weak var good: UIButton!
+    @IBOutlet weak var soso: UIButton!
+    @IBOutlet weak var bad: UIButton!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+//        角丸
+        good.layer.cornerRadius = 50.0
+        soso.layer.cornerRadius = 50.0
+        bad.layer.cornerRadius = 50.0
+
 
     }
     
@@ -25,12 +34,42 @@ class todayFeeling: UIViewController {
         let image = UIImage(named: "good")
         
         feelingImage.image = image
+        
+    
+        let alert: UIAlertController = UIAlertController(title: "Good!", message: "今日も一日一歩ずついきましょう！", preferredStyle:  UIAlertController.Style.alert)
+        
+   
+        let defaultAction: UIAlertAction = UIAlertAction(title: "はい", style: UIAlertAction.Style.default, handler:{
+            (action: UIAlertAction!) -> Void in
+            
+        })
+   
+        alert.addAction(defaultAction)
+        
+        present(alert, animated: true, completion: nil)
     }
+    
+    
     
     @IBAction func sosoBtn(_ sender: Any) {
         let image = UIImage(named: "soso")
         
         feelingImage.image = image
+        
+        let alert: UIAlertController = UIAlertController(title: "So so ...", message: "ボチボチいきましょう。", preferredStyle:  UIAlertController.Style.alert)
+        
+        
+        let defaultAction: UIAlertAction = UIAlertAction(title: "はい", style: UIAlertAction.Style.default, handler:{
+            (action: UIAlertAction!) -> Void in
+            
+        })
+        
+        alert.addAction(defaultAction)
+        
+        present(alert, animated: true, completion: nil)
+        
+        
+        
     }
     
     
@@ -38,6 +77,20 @@ class todayFeeling: UIViewController {
         let image = UIImage(named: "bad")
         
         feelingImage.image = image
+        
+        
+        let alert: UIAlertController = UIAlertController(title: "Bad", message: "無理しないでね", preferredStyle:  UIAlertController.Style.alert)
+        
+        
+        let defaultAction: UIAlertAction = UIAlertAction(title: "はい", style: UIAlertAction.Style.default, handler:{
+            (action: UIAlertAction!) -> Void in
+            
+        })
+        
+        alert.addAction(defaultAction)
+        
+        present(alert, animated: true, completion: nil)
+        
     }
     
     
