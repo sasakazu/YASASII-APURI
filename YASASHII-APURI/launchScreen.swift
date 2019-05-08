2405138fe35e0f38eb1e77f7382d70732089f5de
kazuyasasakura <kazuya.sasakura@gmial.com>
2019-05-08 17:45:54 +0900

    next...

--- YASASHII-APURI/launchScreen.swift
+++ YASASHII-APURI/launchScreen.swift
@@ -1,24 +1,24 @@
 //
-//  ViewController.swift
+//  launchScreen.swift
 //  YASASHII-APURI
 //
-//  Created by 笹倉一也 on 2019/04/14.
+//  Created by 笹倉一也 on 2019/05/08.
 //  Copyright © 2019 笹倉一也. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-    
-    
-    
-    
+class launchScreen: UIViewController {
 
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+    }
+    
+
+ 
 
 }
-
